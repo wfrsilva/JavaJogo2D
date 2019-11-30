f5a00694d27f89452fd6716d4515027c63d44f09
Wendel Fabiano Ribeiro da Silva <wfrsilva@gmail.com>
2019-11-30 00:08:18 -0300

    Update Container.java

--- Container.java
+++ Container.java
@@ -4,19 +4,17 @@ import javax.swing.JFrame;
 
 public class Container extends JFrame{
   
-    public Container (){
-    
-      setTitle("Meu jogo");
-      setSize(1024,728);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setLocationRelativeTo(null);
-      this.setResizable(false);
-      setVisible(true);
-
-    }// constructor
+	public Container (){
+    		setTitle("Meu jogo");
+		setSize(1024,728);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		this.setResizable(false);
+		setVisible(true);
+	}// constructor
              
 	public static void main (String [] args){
-      new Container();
+		new Container();
 	}// main
     
   
