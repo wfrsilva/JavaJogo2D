ac5d9d8ccd97ecb9ae187812b5e74bb4302d00a5
Wendel Fabiano Ribeiro da Silva <wfrsilva@gmail.com>
2019-11-30 00:06:52 -0300

    Create Container.java

---
+++ Container.java
@@ -0,0 +1,23 @@
+package meujogo;
+
+import javax.swing.JFrame;
+
+public class Container extends JFrame{
+  
+    public Container (){
+    
+      setTitle("Meu jogo");
+      setSize(1024,728);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setLocationRelativeTo(null);
+      this.setResizable(false);
+      setVisible(true);
+
+    }// constructor
+             
+	public static void main (String [] args){
+      new Container();
+	}// main
+    
+  
+}//class Container
