5c302081102c9619251222dd2a92eee1dd2f8eed
Wendel Fabiano Ribeiro da Silva <wfrsilva@gmail.com>
2019-12-08 22:08:31 -0300

    update: Inimigo1.java criado
    Inimigo1.java implantado

--- src/meujogo/modelo/Fase.java
+++ src/meujogo/modelo/Fase.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -18,6 +19,7 @@ public class Fase extends JPanel implements ActionListener{
 	private Image nebulosa;
 	private Player player;
 	private  Timer timer;
+	private List<Inimigo1> inimigo1;
 	
 	public Fase() {
 		setFocusable(true);
@@ -36,8 +38,22 @@ public class Fase extends JPanel implements ActionListener{
 		timer = new Timer(5, this);
 		timer.start();
 		
+		inicializaInimigos();
+		
 		
 	}//constructor
+	
+	public void inicializaInimigos() {
+		int coordenadas [] = new int [40];
+		inimigo1 = new ArrayList<Inimigo1>();
+		
+		for (int i = 0; i < coordenadas.length; i++) {
+			int x  = (int)(Math.random() * 8000+1024);
+			int y =  (int)(Math.random() * 650+30);
+			inimigo1.add(new Inimigo1(x,y));
+		}//for
+		
+	}//inicializaInimigos
 	
 	public void paint(Graphics g) {
 		Graphics2D graficos = (Graphics2D) g;
@@ -52,7 +68,13 @@ public class Fase extends JPanel implements ActionListener{
 			graficos.drawImage(m.getImagem(), m.getX(), m.getY(), this);
 		}//for
 		
-		graficos.drawImage(player.getImagem(), player.getX(), player.getY(), this);
+		graficos.drawImage(player.getImagem(), player.getX(), player.getY(), this); ///inverti aqui com o original, para  a nave ficar aciam do tro, visualmente fica mais bonito
+		
+		for (int o = 0; o < inimigo1.size(); o++) {
+			Inimigo1 in = inimigo1.get(o);
+			in.load();
+			graficos.drawImage(in.getImagem(), in.getX(),in.getY(), this);
+		}//for
 		
 		g.dispose();
 	}// paint
@@ -70,6 +92,17 @@ public class Fase extends JPanel implements ActionListener{
 				else {
 					tiros.remove(i);
 				}//else
+			}//for
+			
+			for (int o = 0; o < inimigo1.size(); o++) {
+				Inimigo1 in = inimigo1.get(o);
+				if(in.isVisivel()) {
+					in.update();
+				}//if
+				else {
+					inimigo1.remove(o);
+				}//else
+				
 			}//for
 			
 			repaint();
---
+++ src/meujogo/modelo/Inimigo1.java
@@ -0,0 +1,69 @@
+package meujogo.modelo;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Inimigo1 {
+	
+	private Image imagem;
+	private int x, y;
+	private int largura, altura;
+	private boolean isVisivel;
+	
+	// private static final int LARGURA = 938;
+	private static int VELOCIDADE = 2;
+	
+	public Inimigo1 (int x, int y) {
+		this.x = x;
+		this.y = y;
+		isVisivel = true;
+	}//constructor
+	
+	public void load() {
+		ImageIcon referencia = new ImageIcon("res\\inimigo1.png");
+		imagem = referencia.getImage();
+		
+		this.largura = imagem.getWidth(null);
+		this.altura = imagem.getHeight(null);
+	}//load
+	
+	public void update() {
+		this.x -= VELOCIDADE;
+		//if (this.x > LARGURA) {
+			//isVisivel = false;
+		//}//if
+	}//update
+	
+
+	public boolean isVisivel() {
+		return isVisivel;
+	}
+
+	public void setVisivel(boolean isVisivel) {
+		this.isVisivel = isVisivel;
+	}
+
+	public static int getVELOCIDADE() {
+		return VELOCIDADE;
+	}
+
+	public static void setVELOCIDADE(int vELOCIDADE) {
+		VELOCIDADE = vELOCIDADE;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Image getImagem() {
+		return imagem;
+	}
+	
+	
+	
+}//class
