1fce07e4e0e99a735a75761a97aed6839f1a1de5
Wendel Fabiano Ribeiro da Silva <wfrsilva@gmail.com>
2019-12-09 00:47:24 -0300

    update: JFrame icone
    Inclusão do icone no JFrame

--- src/meujogo/Container.java
+++ src/meujogo/Container.java
@@ -1,5 +1,9 @@
 package meujogo;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 import meujogo.modelo.Fase;
@@ -8,7 +12,10 @@ public class Container extends JFrame{
   
 	public Container (){
 		add(new Fase());
-    	setTitle("Meu jogo");
+    	setTitle("Meu jogo de Espaconave");
+    	ImageIcon icone = new ImageIcon("res//icon_spaceship.png");
+        this.setIconImage(icone.getImage());
+
 		setSize(1024,728);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
@@ -20,5 +27,6 @@ public class Container extends JFrame{
 		new Container();
 	}// main
     
+	
   
 }//class Container
